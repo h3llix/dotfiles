d884a824af59870f65f2084d7593ec1e9fe792d4
Gaurav Genani <h3llix.pvt@gmail.com>
2022-06-24 08:33:58 +0000

    scripts: fix bw variables

--- .chezmoiscripts/run_onchange_before_install-bitwarden.sh
+++ .chezmoiscripts/run_onchange_before_install-bitwarden.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-{{- if .personal -}}
+{{ if .personal -}}
 wget https://github.com/bitwarden/cli/releases/download/v1.22.1/bw-linux-1.22.1.zip
 apt install -y unzip
 unzip bw-linux-1.22.1.zip
 chmod +x ./bw
 mv ./bw /usr/local/bin
 bw login {{ .bwemail }} {{ .bwpass }}
-{{- end -}}
+{{ end -}}
