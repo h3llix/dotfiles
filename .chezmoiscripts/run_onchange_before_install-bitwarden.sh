46de0a3655491b0d1f83899f14d5ca2bf975e9f5
Gaurav Genani <h3llix.pvt@gmail.com>
2022-06-24 07:29:00 +0000

    scripts: change to install when personal only

--- .chezmoiscripts/run_onchange_before_install-bitwarden.sh
+++ .chezmoiscripts/run_onchange_before_install-bitwarden.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+{{- if .personal -}}
 wget https://github.com/bitwarden/cli/releases/download/v1.22.1/bw-linux-1.22.1.zip
 apt install -y unzip
 unzip bw-linux-1.22.1.zip
 chmod +x ./bw
 mv ./bw /usr/local/bin
-
+{{- end -}}
